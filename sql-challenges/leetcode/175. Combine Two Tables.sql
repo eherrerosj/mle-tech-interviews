0932edf717b16e39c35e4d1bc4735e2b95031319
Enrique Herreros <kikexclusive@gmail.com>
2021-03-17 09:58:23 +0100

    chore: add easy sql leetcode solution

---
+++ sql-challenges/leetcode/175. Combine Two Tables.sql
@@ -0,0 +1,48 @@
+/*
+ Create table Person (PersonId int, FirstName varchar(255), LastName varchar(255))
+ Create table Address (AddressId int, PersonId int, City varchar(255), State varchar(255))
+ Truncate table Person
+ insert into Person (PersonId, LastName, FirstName) values ('1', 'Wang', 'Allen')
+ Truncate table Address
+ insert into Address (AddressId, PersonId, City, State) values ('1', '2', 'New York City', 'New York')
+ 
+ able: Person
+ 
+ +-------------+---------+
+ | Column Name | Type    |
+ +-------------+---------+
+ | PersonId    | int     |
+ | FirstName   | varchar |
+ | LastName    | varchar |
+ +-------------+---------+
+ PersonId is the primary key column for this table.
+ 
+ Table: Address
+ 
+ +-------------+---------+
+ | Column Name | Type    |
+ +-------------+---------+
+ | AddressId   | int     |
+ | PersonId    | int     |
+ | City        | varchar |
+ | State       | varchar |
+ +-------------+---------+
+ AddressId is the primary key column for this table.
+ 
+ 
+ 
+ Write a SQL query for a report that provides the following information for each person in the Person table, regardless if there is an address for each of those people:
+ 
+ FirstName, LastName, City, State
+ 
+ Learnings:
+ - Just use join, left because we want every person even if they don't have an address associated
+ */
+select
+    FirstName,
+    LastName,
+    City,
+    State
+from
+    Person p
+    left join Address a on a.PersonId = p.PersonId;
