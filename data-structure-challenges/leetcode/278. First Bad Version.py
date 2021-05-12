fc6868ed0d83d7c0f3fadc75906446964826e8ee
Enrique Herreros <kikexclusive@gmail.com>
2021-05-13 00:17:10 +0200

    chore: solve LC 278 - first bad version

---
+++ data-structure-challenges/leetcode/278. First Bad Version.py
@@ -0,0 +1,54 @@
+"""
+You are a product manager and currently leading a team to develop a new product.
+Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one,
+which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which returns whether version is bad.
+Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+Example 1:
+Input: n = 5, bad = 4
+Output: 4
+Explanation:
+call isBadVersion(3) -> false
+call isBadVersion(5) -> true
+call isBadVersion(4) -> true
+Then 4 is the first bad version.
+
+Example 2:
+Input: n = 1, bad = 1
+Output: 1
+
+Constraints:
+    1 <= bad <= n <= 231 - 1
+
+Learnings:
+- Solved with binary search. O(logn) since the search space is halved at every iteration
+- In order to avoid overflow if left + right > MAX_INT, better to update mid with left + (right-left) / 2
+"""
+
+from typing import List
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return an integer
+# def isBadVersion(version):
+
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left = 0
+        right = n
+        while left < right:
+            mid = left + (right - left) // 2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
